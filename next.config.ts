8027ae186dedcf31ab682fbf45bd77ef82530133
Platon W <woxler.platon@gmail.com>
2024-11-16 11:27:01 +0100

    fix(build): don't build to github pages  + build dynamic routes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: 'out',
-  output: "export",
   images: {
     unoptimized: true,
     remotePatterns: [
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { client } from "@/sanity/client";
 import { HERO_QUERY, POSTS_QUERY } from "@/sanity/constants";
 import { SanityDocument } from "next-sanity";
@@ -5,33 +6,48 @@ import { About } from "./components/About";
 import { Contact } from "./components/Contact";
 import { Hero } from "./components/Hero";
 import { Work } from "./components/Work";
+import { useEffect, useState } from "react";
 
 const options = { next: { revalidate: 5 } };
 
-export default async function Home() {
-  const heroContent = await client.fetch<SanityDocument>(
-    HERO_QUERY,
-    {},
-    options
-  );
+export default function Home() {
+  const [heroContent, setHeroContent] = useState<SanityDocument>();
+  const [posts, setPosts] = useState<SanityDocument[]>();
 
-  const workPosts = await client.fetch<SanityDocument[]>(
-    POSTS_QUERY,
-    {},
-    options
-  );
+  useEffect(() => {
+    const getPosts = async () => {
+      const res: SanityDocument[] = await client.fetch<SanityDocument[]>(
+        POSTS_QUERY,
+        {},
+        options
+      );
+      setPosts(res);
+    };
+    const getHeroData = async () => {
+      const res: SanityDocument = await client.fetch<SanityDocument>(
+        HERO_QUERY,
+        {},
+        options
+      );
+      setHeroContent(res);
+    };
+    getPosts();
+    getHeroData();
+  }, []);
 
-  console.log("heroContent.imgUrl: ", heroContent.imageUrl);
   return (
-    <>
-      <section className="px-16 mx-auto">
-        {heroContent.imageUrl && <Hero imageUrl={heroContent.imageUrl} />}
-      </section>
-      <section className="mx-auto">
-        <About heroContent={heroContent} />
-        <Work posts={workPosts} />
-        <Contact />
-      </section>
-    </>
+    heroContent &&
+    posts && (
+      <>
+        <section className="px-16 mx-auto">
+          <Hero imageUrl={heroContent.imageUrl} />
+        </section>
+        <section className="mx-auto">
+          <About heroContent={heroContent} />
+          <Work posts={posts} />
+          <Contact />
+        </section>
+      </>
+    )
   );
 }
